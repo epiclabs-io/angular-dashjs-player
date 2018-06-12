b2c98a4f8a7c46d04abfec16bc469c60cf2b58e3
Alvaro Escarcha <alvaro.escarcha@gmail.com>
2018-06-12 16:20:07 +0200

    Loading dashjs player and mpd

--- src/lib/dashjs-player.component.ts
+++ src/lib/dashjs-player.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {MediaPlayer, MediaPlayerClass} from 'dashjs';
 
 @Component({
   selector: 'dashjs-player',
-  template: `
-    <p>
-      dashjs-player works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './dashjs-player.component.html',
+  styleUrls: ['./dashjs-player.component.css']
 })
 export class DashjsPlayerComponent implements OnInit {
+  @ViewChild('videoPlayer') videoPlayer: ElementRef;
+  @Input() autoload: boolean = true;
+  @Input() src: string;
+
+  public player: MediaPlayerClass;
 
   constructor() { }
 
   ngOnInit() {
+    this.player = MediaPlayer().create();
+    this.player.initialize(this.videoPlayer.nativeElement, this.src, this.autoload);
   }
 
 }
