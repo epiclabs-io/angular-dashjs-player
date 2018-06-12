f8fb2c124136c3844141d62467aa13d33aacda19
Alvaro Escarcha <alvaro.escarcha@gmail.com>
2018-06-12 16:50:57 +0200

    Added functionality to play, pause and get the player, also watching src changes

--- src/lib/dashjs-player.component.ts
+++ src/lib/dashjs-player.component.ts
@@ -7,17 +7,35 @@ import {MediaPlayer, MediaPlayerClass} from 'dashjs';
   styleUrls: ['./dashjs-player.component.css']
 })
 export class DashjsPlayerComponent implements OnInit {
+  public player: MediaPlayerClass;
+  private _src: string;
+
   @ViewChild('videoPlayer') videoPlayer: ElementRef;
   @Input() autoload: boolean = true;
-  @Input() src: string;
-
-  public player: MediaPlayerClass;
+  @Input() set src(value: string) {
+    this._src = value;
+    if (this.player && this.player.isPaused()) {
+      this.play();
+    }
+  }
 
   constructor() { }
 
   ngOnInit() {
     this.player = MediaPlayer().create();
-    this.player.initialize(this.videoPlayer.nativeElement, this.src, this.autoload);
+    this.player.initialize(this.videoPlayer.nativeElement, this._src, this.autoload);
+  }
+
+  public getPlayer(): MediaPlayerClass {
+    return this.player;
+  }
+
+  public play(): void {
+    this.player.play();
+  }
+
+  public pause(): void {
+    this.player.pause();
   }
 
 }
